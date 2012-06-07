29b09e96fdee92cca2c935dc68bde58e765adacc
Steven Chitwood <schitwood@cogswell.edu>
2012-06-07 15:24:12 -0700

    answers doc

--- Answers.py
+++ Answers.py
@@ -1,10 +1,14 @@
 class Answer: pass
 
+__doc__ = """An answer class that returns the time."""
 class TimeAnswer( Answer ):
 
     def __init__( self, time ):
         self.time = time
-        
+
+__doc__ = """An answer class that returns the original quesion from the Client
+as an answer."""
 class EchoAnswer( Answer ):
+    
     def __init__( self, object ):
         self.object = object
--- Clients.py
+++ Clients.py
@@ -1,3 +1,6 @@
+__doc__ = """The Client class returns the answer back to the client that was
+passed through connection from the server."""
+
 class Client:
 
     def getAnswer( self, question ):
