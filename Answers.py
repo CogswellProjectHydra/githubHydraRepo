b5eb08d36ba75e12af6d33bff03f3ac816f1ba68
Steven Chitwood <schitwood@cogswell.edu>
2012-06-07 16:39:18 -0700

    Fixed comments

--- Answers.py
+++ Answers.py
@@ -1,14 +1,17 @@
+__doc__ = """The Answers module represents any answer that is given."""
 class Answer: pass
 
-__doc__ = """An answer class that returns the time."""
+
 class TimeAnswer( Answer ):
+    """An answer class that returns the time."""
 
     def __init__( self, time ):
         self.time = time
 
-__doc__ = """An answer class that returns the original quesion from the Client
-as an answer."""
+
 class EchoAnswer( Answer ):
+    """An answer class that returns the original quesion from the Client
+    as an answer."""
     
     def __init__( self, object ):
         self.object = object
--- Questions.py
+++ Questions.py
@@ -2,17 +2,25 @@ import time
 
 from Answers import Answer, TimeAnswer, EchoAnswer
 
+__doc__ = """This class is the base class of both the TimeQuestion and
+EchoQuestion class. The Quesiton class represents of how any answer is
+returned."""
 class Question:
     
     def computeAnswer( self, server ):
         return Answer( )
-    
+
 class TimeQuestion( Question ):
+    """TimeQuestion corresponds with TimeAnswer in the Answers module,
+    returning the answer."""
      
     def computeAnswer( self, server ):
         return TimeAnswer( time.localtime( ) )
 
+
 class EchoQuestion( Question ):
+    """EchoQuestion corresponds with EchoAnswer in the Answers module,
+    returning the answer."""
 
     def __init__( self, object ):
         self.object = object
