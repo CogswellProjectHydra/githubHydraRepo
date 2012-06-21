f94f8390cf1aa3560a0e3d586921c0c0d44920e9
David Gladstein <dgladstein@cogswell.edu>
2012-06-21 14:43:16 -0700

    cleaned up test a little

--- DjangoTest.py
+++ DjangoTest.py
@@ -3,8 +3,11 @@ import DjangoSetup
 from Hydra.models import RenderTask
 
 RenderTask( command = 'blammo' ).save( )
+
 for rt in RenderTask.objects.all( ):
     print rt
+
 print RenderTask.objects.count( ), 'objects found'
-print RenderTask.objects.get( id = 3 )
+
+RenderTask.objects.filter( command__contains = 'blammo' ).delete( )
 
