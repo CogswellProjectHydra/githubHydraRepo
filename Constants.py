89b8276015511694bbe4ea0f0495cf92b5e4fdc5
David Gladstein <dgladstein@cogswell.edu>
2012-06-26 10:46:42 -0700

    put hostname back to localhost by default.

--- Constants.py
+++ Constants.py
@@ -1,5 +1,6 @@
 PORT = 8765
-HOSTNAME = "154-04"
+HOSTNAME = "localhost" # for testing on a single machine
+#HOSTNAME = "154-01"    # for testing on another machine
 MANYBYTES = 1 << 20
 
 __doc__= """This just defines our port and sets limitations on the bytes available"""
