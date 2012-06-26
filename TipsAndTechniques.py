5cf034aefce3768e617059cd8353b0c6afd6d5e1
David Gladstein <dgladstein@cogswell.edu>
2012-06-26 12:00:45 -0700

    techniques for running commands with log files.

---
+++ TipsAndTechniques.py
@@ -0,0 +1,77 @@
+from LoggingSetup import logger
+import subprocess
+import os
+
+def toRunMaya( ):
+
+    command = [r"c:\Program Files\Autodesk\Maya2011\bin\Render.exe",
+               "-help",
+               ]
+
+    # it's easier to supply the "words" on a command line as individual strings
+    # rather than glueing them together into one big string.
+    # the subprocess module's functions let you do either.
+    return subprocess.call (command)
+
+def withOutputToFile( filename, command ):
+    "run a command, sending output to a file"
+
+    logger.debug( 'writing log file %s', filename )
+
+    with file( filename, 'w' ) as f:
+        f.write( "Launching command %r\n\n" % command )
+        f.flush( )
+        # the 'with' statement closes f if there's an error
+        return subprocess.call( command,
+                                stdout = f,
+                                stderr = subprocess.STDOUT,
+                                )
+    
+def listRenderers(  ):
+
+    command = [
+               r"c:\Program Files\Autodesk\Maya2011\bin\Render.exe",
+               "-listRenderers",
+               ]
+
+    tempDir = os.getenv( 'TMP')
+    logFile = os.path.join( tempDir, 'logfile1.txt' )
+    return withOutputToFile( logFile, command )
+
+def missingArgument(  ):
+
+    command = [r"c:\Program Files\Autodesk\Maya2011\bin\Render.exe",
+               
+               ]
+
+    tempDir = os.getenv( 'TMP')
+    logFile = os.path.join( tempDir, 'logfile2.txt' )
+    return withOutputToFile( logFile, command )
+
+def commandNameError(  ):
+
+    command = ["cmd",
+               "/c",
+               r"c:\Program Files\Autodesk\Maya2011\bin\Render.exeeeeeeee",
+                "-help"               
+               ]
+    # using cmd /c to run the command starts a new shell. That way if the program name is wrong, the shell
+    # will complain about it, but the subprocess.call won't fail. Which it would, if you simply passed it a bad
+    # command to launch.
+    
+
+    tempDir = os.getenv( 'TMP')
+    logFile = os.path.join( tempDir, 'logfile3.txt' )
+    return withOutputToFile( logFile, command )
+
+
+if __name__ == '__main__':
+
+    logger.debug( 'return code %s', toRunMaya( ) )
+
+    logger.debug( 'return code %s', listRenderers( ) )
+    
+    logger.debug( 'return code %s', missingArgument( ) )
+    
+    logger.debug( 'return code %s', commandNameError( ) )
+    
