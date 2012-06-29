fd106de148d3362e9d5c13abcf2779634d944c17
David Gladstein <dgladstein@cogswell.edu>
2012-06-29 08:19:00 -0700

    Added actualMayaRender

--- TipsAndTechniques.py
+++ TipsAndTechniques.py
@@ -1,6 +1,7 @@
 from LoggingSetup import logger
 import subprocess
 import os
+import traceback
 
 def toRunMaya( ):
 
@@ -64,6 +65,36 @@ def commandNameError(  ):
     logFile = os.path.join( tempDir, 'logfile3.txt' )
     return withOutputToFile( logFile, command )
 
+def actualMayaRender( ):
+    command = [
+           r'c:\program files\autodesk\maya2011\bin\render.exe',
+           
+           '-mr:v', '5',
+           # we need verbose output to see where the
+           # render went and to get progress during long renders
+           
+           r'c:\users\gladstein\desktop\chair2.ma',
+           ]
+    logFileName = r'c:\temp\test.log.txt'
+    log = file( logFileName, 'w' )
+
+    try:
+        log.write( 'Hydra log file %s on %s\n' % ( logFileName, os.environ['COMPUTERNAME'] ) )
+        # identify the log file in the log file itself, so if it gets emailed you know where it came from.
+        
+        log.write( 'Command is %r\n\n' % ( command ) )
+        # put the command arguments in the log file, using %r so we can see
+
+        log.flush( )
+        
+        return subprocess.call( command, stdout=log, stderr=subprocess.STDOUT )
+    except Exception, e:
+        traceback.print_exc( e, log )
+        raise
+    finally:
+        log.close( )
+
+
 
 if __name__ == '__main__':
 
@@ -74,4 +105,7 @@ if __name__ == '__main__':
     logger.debug( 'return code %s', missingArgument( ) )
     
     logger.debug( 'return code %s', commandNameError( ) )
+
+    logger.debug( 'return code %s', actualMayaRender( ) )
+    
     
