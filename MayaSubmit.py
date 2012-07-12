63584cda6d33506fbef65ba36107c291cc49ec11
David Gladstein <dgladstein@cogswell.edu>
2012-07-12 14:50:47 -0700

    running submitter from maya

---
+++ MayaSubmit.py
@@ -0,0 +1,11 @@
+from maya import cmds
+import subprocess
+
+subprocess.check_call(['cmd', '/c', 'start', r'c:\python27\python.exe',
+    r'\\flex2\projectHydra\githubHydraRepo\submitterMain.py',
+#    r'u:\hydraRepo\submitterMain.py',
+    cmds.file( query = True, location = True ),
+    str( cmds.getAttr( 'defaultRenderGlobals.startFrame' ) ),
+    str( cmds.getAttr( 'defaultRenderGlobals.endFrame' ) ),
+    str( cmds.getAttr( 'defaultRenderGlobals.byFrame' ) ),
+    ])
--- SubmitterMain.py
+++ SubmitterMain.py
@@ -21,18 +21,48 @@ class SubmitterWindow( QMainWindow, Ui_MainWindow ):
 
         QObject.connect(self.submitButton, SIGNAL("clicked()"), self.doSubmit)
 
-        scene, start, end, by = sys.argv[1:]
+        scene, start, end, by = sys.argv[1:] # proper command line args would be nice
+        self.sceneLabel.setText( scene )
+        self.startSpinBox.setValue( eval (start ) )
+        self.endSpinBox.setValue( eval( end ) )
         
 
     def doSubmit( self ):
         logger.debug ('doSubmit')
 
-if __name__ == '__main__':
-    logger.debug(sys.argv)
-    app = QApplication( sys.argv )
-    
-    window = SubmitterWindow( )
+        sceneFile = str( self.sceneLabel.text( ) )
+        startFrame = self.startSpinBox.value( )
+        endFrame = self.endSpinBox.value( )
+        batchSize = self.batchSizeSpinBox.value( )
 
-    window.show( )
-    retcode = app.exec_( )
-    sys.exit( retcode )        
+        starts = range( startFrame, endFrame + 1, batchSize )
+        ends = [min( start + batchSize - 1,
+                     endFrame )
+                for start in starts
+                ]
+        logger.debug( zip( starts, ends ) )
+        for start, end in zip( starts, ends ):
+            command = [
+                        r'c:\program files\autodesk\maya2011\bin\render.exe',
+                        '-mr:v', '5',
+                        '-s', str( start ),
+                        '-e', str( end ),
+                        sceneFile
+                      ]
+            logger.debug( command )
+
+if __name__ == '__main__':
+    try:
+        logger.debug(sys.argv)
+        app = QApplication( sys.argv )
+        
+        window = SubmitterWindow( )
+
+        window.show( )
+        retcode = app.exec_( )
+        sys.exit( retcode )        
+    except Exception, e:
+        logger.error( traceback.format_exc( e ) )
+        raise
+    
+                      
