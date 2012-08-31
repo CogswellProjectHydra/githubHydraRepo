3ac01e8f80546d14d51e248f4fd3876bbf2bc1e2
David Gladstein <dgladstein@cogswell.edu>
2012-08-30 19:07:35 -0700

    Put quotes around scene name in case of funny characters;

--- MayaSubmit.py
+++ MayaSubmit.py
@@ -2,9 +2,9 @@ from maya import cmds
 import subprocess
 
 subprocess.check_call(['cmd', '/c', 'start', r'c:\python27\python.exe',
-    r'\\flex2\projectHydra\githubHydraRepo\submitterMain.py',
+    r'\\flex2.cpc.local\projectHydra\githubHydraRepo\submitterMain.py',
 #    r'u:\hydraRepo\submitterMain.py',
-    cmds.file( query = True, location = True ),
+    '"' + cmds.file( query = True, location = True ) + '"',
     str( cmds.getAttr( 'defaultRenderGlobals.startFrame' ) ),
     str( cmds.getAttr( 'defaultRenderGlobals.endFrame' ) ),
     str( cmds.getAttr( 'defaultRenderGlobals.byFrame' ) ),
